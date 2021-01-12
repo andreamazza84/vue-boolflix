cae349d83550e6f6162facecddf2cca78179aafc
andreamazza84 <andrea.mazza84@gmail.com>
2021-01-12 18:49:41 +0100

    Milestone-2 - Bandiere inserite

--- Milestone-2/main.js
+++ Milestone-2/main.js
@@ -20,27 +20,196 @@
 // Trasformiamo
 // il voto da 1 a 10 decimale in un numero intero da 1 a 5, così dapermetterci di stampare
 // a schermo un numero di stelle piene che vanno da 1 a 5,lasciando le restanti vuote 
-//(troviamo le icone in FontAwesome).
-// Arrotondiamo sempre per eccesso all’unità successiva, 
-//non gestiamo icone mezzepiene (o mezze vuote :P)Trasformiamo poi la stringa statica della
-// lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in 
-//cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in
-// FontAwesome).Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di
+// (troviamo le icone in FontAwesome).
+// Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezzepiene (o mezze vuote :P)
+// Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera 
+// della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della
+// nazione ritornata dall’API (le flag non ci sono in FontAwesome).
+// Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di
 // ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, 
-//stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel
+// stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel
 // JSON di risposta diversi, simili ma non sempre identici). 
 // Qui un esempio di chiamata per le serie tv:
-//https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=scrubs
+// https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=scrubs
+
+
+
 let app = new Vue({
     el: '#root',
     data: {
         search: '',
         movies: [],
         moviesMap: [],
+        flagCodeMap: {
+            "aa": "dj",
+            "af": "za",
+            "ak": "gh",
+            "sq": "al",
+            "am": "et",
+            "ar": {
+                "proposed_iso_3166": "aa",
+                "flag": "https:\/\/en.wikipedia.org\/wiki\/Flag_of_the_Arab_League",
+                "name": "Arab League"
+            },
+            "hy": "am",
+            "ay": {
+                "proposed_iso_3166": "wh",
+                "flag": "https:\/\/en.wikipedia.org\/wiki\/Wiphala",
+                "name": "Wiphala"
+            },
+            "az": "az",
+            "bm": "ml",
+            "be": "by",
+            "bn": "bd",
+            "bi": "vu",
+            "bs": "ba",
+            "bg": "bg",
+            "my": "mm",
+            "ca": "ad",
+            "zh": "cn",
+            "hr": "hr",
+            "cs": "cz",
+            "da": "dk",
+            "dv": "mv",
+            "nl": "nl",
+            "dz": "bt",
+            "en": "gb",
+            "et": "ee",
+            "ee": {
+                "proposed_iso_3166": "ew",
+                "flag": "https:\/\/en.wikipedia.org\/wiki\/Ewe_people#\/media\/File:Flag_of_the_Ewe_people.svg",
+                "name": "Ewe"
+            },
+            "fj": "fj",
+            "fil": "ph",
+            "fi": "fi",
+            "fr": "fr",
+            "ff": {
+                "proposed_iso_3166": "ff",
+                "flag": "https:\/\/www.nationstates.net\/images\/flags\/uploads\/fulah__403173.png",
+                "name": "Fulah"
+            },
+            "gaa": "gh",
+            "ka": "ge",
+            "de": "de",
+            "el": "gr",
+            "gn": {
+                "proposed_iso_3166": "gx",
+                "flag": "https:\/\/www.crwflags.com\/fotw\/flags\/xg.html",
+                "name": "Guarani"
+            },
+            "gu": "in",
+            "ht": "ht",
+            "ha": {
+                "proposed_iso_3166": "ha",
+                "flag": "https:\/\/www.crwflags.com\/fotw\/flags\/ng%7Dhausa.html",
+                "name": "Hausa"
+            },
+            "he": "il",
+            "hi": "in",
+            "ho": "pg",
+            "hu": "hu",
+            "is": "is",
+            "ig": "ng",
+            "id": "id",
+            "ga": "ie",
+            "it": "it",
+            "ja": "jp",
+            "kr": "ne",
+            "kk": "kz",
+            "km": "kh",
+            "kmb": "ao",
+            "rw": "rw",
+            "kg": "cg",
+            "ko": "kr",
+            "kj": "ao",
+            "ku": "iq",
+            "ky": "kg",
+            "lo": "la",
+            "la": "va",
+            "lv": "lv",
+            "ln": "cg",
+            "lt": "lt",
+            "lu": "cd",
+            "lb": "lu",
+            "mk": "mk",
+            "mg": "mg",
+            "ms": "my",
+            "mt": "mt",
+            "mi": "nz",
+            "mh": "mh",
+            "mn": "mn",
+            "mos": "bf",
+            "ne": "np",
+            "nd": "zw",
+            "nso": "za",
+            "no": "no",
+            "nb": "no",
+            "nn": "no",
+            "ny": "mw",
+            "pap": "aw",
+            "ps": "af",
+            "fa": "ir",
+            "pl": "pl",
+            "pt": "pt",
+            "pa": "in",
+            "qu": "wh",
+            "ro": "ro",
+            "rm": "ch",
+            "rn": "bi",
+            "ru": "ru",
+            "sg": "cf",
+            "sr": "rs",
+            "srr": "sn",
+            "sn": "zw",
+            "si": "lk",
+            "sk": "sk",
+            "sl": "si",
+            "so": "so",
+            "snk": "sn",
+            "nr": "za",
+            "st": "ls",
+            "es": "es",
+            "sw": {
+                "proposed_iso_3166": "sw",
+                "flag": "https:\/\/commons.wikimedia.org\/wiki\/File:Flag_of_Swahili.gif",
+                "name": "Swahili"
+            },
+            "ss": "sz",
+            "sv": "se",
+            "tl": "ph",
+            "tg": "tj",
+            "ta": "lk",
+            "te": "in",
+            "tet": "tl",
+            "th": "th",
+            "ti": "er",
+            "tpi": "pg",
+            "ts": "za",
+            "tn": "bw",
+            "tr": "tr",
+            "tk": "tm",
+            "uk": "ua",
+            "umb": "ao",
+            "ur": "pk",
+            "uz": "uz",
+            "ve": "za",
+            "vi": "vn",
+            "cy": "gb",
+            "wo": "sn",
+            "xh": "za",
+            "yo": {
+                "proposed_iso_3166": "yo",
+                "flag": "https:\/\/www.crwflags.com\/fotw\/flags\/ng%7Dyorub.html",
+                "name": "Yoruba"
+            },
+            "zu": "za"
+        },
+        //flag
     },
     methods: {
         //Richiesta API per popolare la lista dei film che corrispondono alla ricerca
-        send: function(){
+        get: function(){
             const self = this;
             const search = this.search;
             if (search === '' || search === null || search === NaN) {
@@ -63,31 +232,49 @@ let app = new Vue({
             axios(config)
             .then(function (response) {
                 const movies = response.data.results;
+                self.movies = movies;
+                self.mapList(movies); 
                 //MS-2 - Conversione del voto  
-                const moviesMap = movies.map(element=>{
-                    let vote = element.vote_average;
-                    //conversione in array
-                    element.vote_average = [0, 0, 0, 0, 0]; 
-                    vote = Math.ceil(vote/2);
-                    //popolamento array voto
-                    for (let index = 0; index < (vote-1) ; index++) {
-                        element.vote_average[index] = 1;
-                    }
-                    return element;
-                });//map
-                self.moviesMap = moviesMap;
-                //console.log(self.moviesMap);
+                
             })//then
             .catch(function (error) {
                 console.log(error);
             })//catch
-        }
+
+        },
+        mapList: function(list){
+            const mapList = list.map(element=>{
+                let vote = element.vote_average;
+                let lang = element.original_language;
+
+                //conversione in array
+                element.vote_average = [0, 0, 0, 0, 0];
+                //Conversione ISO 639-1 > ISO 3166-1-alpha-2 code 
+                element.original_language = this.flagCodeMap[lang];
+                //Normalizzazione del voto
+                vote = Math.ceil(vote/2);
+                //popolamento array voto
+                for (let index = 0; index < vote ; index++) {
+                    element.vote_average[index] = 1;
+                }
+                return element;
+            });//map
+            console.log(mapList);
+            return this.moviesMap = mapList;
+        },
     },
     created(){
         // for (let index = 0; index < 5; index++) {
         //     this.vote[index] = 0;
         // }
     },
-    mounted(){},
+    mounted(){
+        //console.log(this.flagCodeMap.en);
+    },
+    
 });
+
+
+//Sito per convertire ISO639-1 in bandiera:
+//https://www.unknown.nu/flags/
 
